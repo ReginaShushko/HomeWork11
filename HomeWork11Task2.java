608780f65bf8132602587de6dfff7d49ecc2b878
Regina_Shushko <110686583+ReginaShushko@users.noreply.github.com>
2022-12-08 20:33:32 +0300

    Add files via upload

---
+++ HomeWork11Task2.java
@@ -0,0 +1,53 @@
+package homeWork11;
+
+import java.util.Scanner;
+
+public class HomeWork11Task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        DivideNumbers numbers = new DivideNumbers();
+        System.out.println("Enter divisible: ");
+        int divisible = scanner.nextInt();
+
+        System.out.println("Enter divider: ");
+
+        while (true) {
+            try {
+                int divider = scanner.nextInt();
+                System.out.println("Result of division: " + numbers.divide(divisible, divider));
+                break;
+            } catch (Exception ex) {
+                System.out.println("Enter divider again: ");
+            }
+        }
+    }
+}
+
+interface Calculator {
+
+    int divide(int a, int b) throws DivideByZeroException;
+}
+
+class DivideNumbers implements Calculator {
+
+    @Override
+    public int divide(int a, int b) throws DivideByZeroException {
+        try {
+            return a / b;
+        } catch (ArithmeticException ex) {
+            throw new DivideByZeroException();
+        }
+    }
+}
+
+class DivideByZeroException extends Exception {
+    public DivideByZeroException() {
+        System.out.println("Division by zero.");
+    }
+}
+
+
+
+
+
